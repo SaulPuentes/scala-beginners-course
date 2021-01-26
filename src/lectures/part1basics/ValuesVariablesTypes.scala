334e4cf47e54200b61f68bac0a92f0fe48b10718
SaulPuentes <saul.puentess@gmail.com>
2021-01-25 21:31:14 -0600

    The Absolute Scala Basics

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,28 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+  // VALS ARE INMUTABLE
+
+  // COMPILER can infer types
+
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4612
+  val aLong: Long = 1231231245112312313L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int  = 4
+
+  aVariable = 5 // side effects
+
+}
